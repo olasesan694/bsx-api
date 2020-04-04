579fedd764fe2a614c4c936e4442ee578197f242
kota-education-group <olasesan694@gmail.com>
2020-04-04 17:46:50 -0400

    fixed contactscontroller3

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -38,14 +38,13 @@ return [
             'secret' => env('PUSHER_APP_SECRET'),
             'app_id' => env('PUSHER_APP_ID'),
             'options' => [
-                'cluster' => env('PUSHER_APP_CLUSTER'),
-                'useTLS' => true,
-                'scheme' => 'http',
-                'curl_options' => [
-                    CURLOPT_SSL_VERIFYHOST => 0,
-                    CURLOPT_SSL_VERIFYPEER => 0,
-                ]
+                'cluster' => 'mt1',
+                'useTLS' => true
             ],
+            // 'options' => [
+            //     'cluster' => env('PUSHER_APP_CLUSTER'),
+            //     'useTLS' => true
+            // ],
         ],
 
         'redis' => [
