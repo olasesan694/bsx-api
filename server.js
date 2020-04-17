89e6b77f0a96fbb570e628b427df7050e62a7b0f
kota-education-group <olasesan694@gmail.com>
2020-04-17 14:57:47 -0400

    build for heroku

--- server.js
+++ server.js
@@ -4,8 +4,8 @@ const app = express();
 
 app.use(express.static(__dirname + "/dist/"));
 app.get(/.*/, function(req, res) {
-  res.sendfile(__dirname + "/dist/index.html");
+  res.sendfile(__dirname + "/dist/index.php");
 });
 app.listen(port);
 
-console.log("Server started...");
+console.log("Server started sista...");
