2899dac60c685d921db148df2aec36bb49c7916c
kota-education-group <olasesan694@gmail.com>
2020-04-17 13:25:05 -0400

    init commit

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -38,8 +38,8 @@ return [
             'secret' => env('PUSHER_APP_SECRET'),
             'app_id' => env('PUSHER_APP_ID'),
             'options' => [
-                'cluster' => 'mt1',
-                'useTLS' => true
+                'cluster' => env('PUSHER_APP_CLUSTER'),
+                'encrypted' => true,
             ],
             // 'options' => [
             //     'cluster' => env('PUSHER_APP_CLUSTER'),
