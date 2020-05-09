693e2e5b5fec55cd5954c78aeb01ac67fad684c8
soladime <tubesometric@gmail.com>
2020-05-09 13:53:11 -0400

    init larashop

--- app/Http/Controllers/ContactsController.php
+++ app/Http/Controllers/ContactsController.php
@@ -4,14 +4,11 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-
 use App\User;
-// use App\Auth;
 use App\Message;
 use App\Events\NewMessage;
-use Illuminate\Support\Facades\DB;
-// use App\Http\Controllers\Controller;
-
+// use Illuminate\Support\Facades\DB;
+use DB;
 
 class ContactsController extends Controller
 {
@@ -22,46 +19,68 @@ class ContactsController extends Controller
     public function get()
     {
 
-        // $users = DB::select('SELECT user_id FROM invites WHERE user_id = :id LIMIT 1', ['id' => auth()->id()]);
-        // foreach ($users as $user) {
-        //     $userid = $user->user_id;
-        //     $this->uid = $userid;
-        // }
+        // gets user id (inviter) from the invites table
+        $users = DB::select('SELECT user_id FROM invites WHERE user_id = :id LIMIT 1', ['id' => auth()->id()]);
+        foreach ($users as $user) {
+            $userid = $user->user_id;
+            $this->uid = $userid;
+        }
         
 
-
-        // $friends = DB::select('SELECT friend_id FROM invites WHERE friend_id = :id LIMIT 1', ['id' => auth()->id()]);
-        // foreach ($friends as $friend) {
-        //     $friendid = $friend->friend_id;
-        //     $this->fid = $friendid;
-        // }
+        // gets friend id (invitee) from invites table
+        $friends = DB::select('SELECT friend_id FROM invites WHERE friend_id = :id LIMIT 1', ['id' => auth()->id()]);
+        foreach ($friends as $friend) {
+            $friendid = $friend->friend_id;
+            $this->fid = $friendid;
+        }
         
 
+        // gets the user's contacts if they are logged in
+        if (auth()->id() == $this->uid) {  
 
-        // if (auth()->id() == $this->uid) {
+            $these = DB::select('SELECT friend_id FROM invites WHERE user_id = :id', ['id' => auth()->id()]);
 
-        //     $these = DB::select('SELECT friend_id FROM invites WHERE user_id = :id', ['id' => auth()->id()]);
-
-        //     foreach ($these as $the) {
-        //         $val = $the->friend_id;
-        //         $contacts = DB::select('SELECT * FROM users WHERE id = :id', ['id' => $val]);
-        //     }
+            foreach ($these as $the) {
+                $val = $the->friend_id;
+                $contacts = DB::select('SELECT * FROM users WHERE id = :id', ['id' => $val]);
+            }
            
-        // } else if (auth()->id() == $this->fid) {
+        } else if (auth()->id() == $this->fid) { // gets the friend's contacts if they are logged in 
 
-        //     $estas = DB::select('SELECT user_id FROM invites WHERE friend_id = :id', ['id' => auth()->id()]);
+            $estas = DB::select('SELECT user_id FROM invites WHERE friend_id = :id', ['id' => auth()->id()]);
 
-        //     foreach ($estas as $esta) {
-        //         $value = $esta->user_id;
-        //         $contacts = DB::select('SELECT * FROM users WHERE id = :id', ['id' => $value]);
-        //     }
+            foreach ($estas as $esta) {
+                $value = $esta->user_id;
+                $contacts = DB::select('SELECT * FROM users WHERE id = :id', ['id' => $value]);
+            }
 
-        // } else {
-        //     $contacts = [];
-        // }
+        } else { // else no contacts.
+            $contacts = [];
+        }
 
-        $contacts = User::where('id', '!=', auth()->id())->get();
 
+
+        // get a collection of items where sender_id is the user who sent us a message
+        // and messages_count is the number of unread messages we have from him
+        $unreadIds = Message::select(\DB::raw("'from' as sender_id, count('from') as messages_count"))
+            ->where('to', auth()->id())
+            ->where('read', false)
+            ->groupBy('from')
+            ->get();
+
+        // add an unread key to each contact with the count of unread messages
+        $contacts = $contacts->map(function($contact) use ($unreadIds) {
+            $contactUnread = $unreadIds->where('sender_id', $contact->id)->first();
+
+            $contact->unread = $contactUnread ? $contactUnread->messages_count : 0;
+
+            return $contact;
+        });
+
+
+
+        
+        // returns the contact of the logggin in user.
         return response()->json($contacts);
     }
 
@@ -69,13 +88,9 @@ class ContactsController extends Controller
 
     public function getMessagesFor($id)
     {
-        // Later: mark all messages with the selected contact as read
 
-        // $messages = Message::where('from', $id)->where('to', auth()->id())->get(); // gets all the messages user sent and messages friend sends
-
-        // $messages = Message::where('from', $id)->orWhere('to', $id)->get();
-
-        // Message::where('from', $id)->where('to', auth()->id())->update(['read' => true]);
+        // mark all messages with the selected contact as read
+        Message::where('from', $id)->where('to', auth()->id())->update(['read' => true]);
 
         $messages = Message::where(function($q) use ($id) {
             $q->where('from', auth()->id());
