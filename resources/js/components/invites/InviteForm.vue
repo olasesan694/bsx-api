58d79024af72604b25bd24f6a6ae82ec109e41ef
soladime <tubesometric@gmail.com>
2020-05-09 23:16:26 -0400

    addFriend func in inviteform

--- resources/js/components/invites/InviteForm.vue
+++ resources/js/components/invites/InviteForm.vue
@@ -36,7 +36,8 @@
                     friendPhone: this.friendPhone,
                     friend_id: 0,
                     // url: "http://localhost:8000/accept" + window.location.search
-                    url: "https://connect.brisebox.com/accept" + window.location.search
+                    url: "http://fierce-dawn-63354.herokuapp.com/accept" + window.location.search
+                    // url: "https://connect.brisebox.com/accept" + window.location.search
                 })
                 .then(response => {
                     window.location.href = '/discuss' + window.location.search
