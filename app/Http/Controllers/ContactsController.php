78f351f1c5d19b23fb18a6d0e6452aad5941e2ed
soladime <tubesometric@gmail.com>
2020-05-10 21:13:40 -0400

    contactscontroller  user select data basedddd

--- app/Http/Controllers/ContactsController.php
+++ app/Http/Controllers/ContactsController.php
@@ -22,13 +22,17 @@ class ContactsController extends Controller
 
         // // get all users except the authenticated one
         // $contacts = User::where('id', '!=', auth()->id())->get();
-        $friendIds = DB::select('SELECT friend_id FROM invites WHERE user_id = :id', ['id' => auth()->id()])->take(1);
+        $friendIds = DB::select('SELECT friend_id FROM invites WHERE user_id = :id', ['id' => auth()->id()]);
         // $friendIds = Invite::select(\DB::raw("friend_id"))
         //     ->where('user_id', auth()->id())
         //     ->limit(1)
         //     ->get();
+        foreach ($friendIds as $friendId) {
+            $value = $friendId->friend_id;
+            // $contacts = DB::select('SELECT * FROM users WHERE id = :id', ['id' => $value]);
+            $contacts = User::where('id', $value)->get();
+        }
 
-        $contacts = User::where('id', $friendIds)->get();
 
         // // gets user id (inviter) from the invites table
         // $users = DB::select('SELECT user_id FROM invites WHERE user_id = :id LIMIT 1', ['id' => auth()->id()]);
