106c921533509d939eeae06111cb25bc497d92f5
soladime <tubesometric@gmail.com>
2020-06-23 19:47:55 -0400

    register domain addition11

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -54,7 +54,7 @@
                                     <label for="domain" class="col-md-4 col-form-label text-md-right"> Website URL </label>
 
                                     <div class="col-md-6">
-                                        <input id="domain" type="text" class="form-control @error('domain') is-invalid @enderror" name="domain" value="{{ old('domain') }}" placeholder="https://www.example.com" required autocomplete="domain">
+                                        <input id="domain" type="text" class="form-control @error('domain') is-invalid @enderror" name="domain" value="{{ old('domain') }}" placeholder="https://www.example.com" autocomplete="domain">
 
                                         @error('domain')
                                             <span class="invalid-feedback" role="alert">
