e7b04bb0790a9e9f827f1289055b52d41b455781
soladime <tubesometric@gmail.com>
2020-06-23 19:17:56 -0400

    register domain addition3

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -43,14 +43,14 @@
                                 </div>
 
                                 <div class="form-group row">
-                                    <label for="domain" class="col-md-4 col-form-label text-md-right">{{ Company account? }}</label>
+                                    <label for="domain" class="col-md-4 col-form-label text-md-right"> Company account? </label>
 
                                     <div class="col-md-6">
                                         <input type="checkbox" id="forcompanycheckbox" onclick="showCompanyEmail()">
                                     </div>
                                 </div>
                                 <div id="forcompany" style="display:none" class="form-group row">
-                                    <label for="domain" class="col-md-4 col-form-label text-md-right">{{ Website url }}</label>
+                                    <label for="domain" class="col-md-4 col-form-label text-md-right"> Website url </label>
 
                                     <div class="col-md-6">
                                         <input id="domain" type="text" class="form-control @error('domain') is-invalid @enderror" name="domain" value="{{ old('domain') }}" placeholder="https://www.example.com" required autocomplete="domain">
