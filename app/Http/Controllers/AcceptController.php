5034995744ba541ec5601d6014ed1edc6d85a970
soladime <tubesometric@gmail.com>
2020-06-25 00:23:45 -0400

    invite domain issue8

--- app/Http/Controllers/AcceptController.php
+++ app/Http/Controllers/AcceptController.php
@@ -25,7 +25,8 @@ class AcceptController extends Controller
         $affected = DB::update('UPDATE invites SET friend_id=:sFriend WHERE friend_phone=:sPhone', [
             'sFriend' => auth()->id(), 
             // 'sPhone' => $user->phone
-            'sPhone' => $user->domain
+            'sPhone' => $user->email
+            // 'sPhone' => $user->domain
         ]); 
 
         // // added s.o. - user and inc live chat
